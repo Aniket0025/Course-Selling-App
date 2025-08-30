94d15190589e0a0f4de329d0fa636c8494fd4fe6
Aniket0025 <aniketyadav25012005@gmail.com>
2025-08-30 11:47:25 +0530

    added scripts nodemon

--- routes/admin.js
+++ routes/admin.js
@@ -24,19 +24,19 @@ adminRouter.post("/signin",function(req,res) {
     })    
 })
 
-adminRouter.post("/",function(req,res) {    
+adminRouter.post("/course",function(req,res) {    
     res.json({
         message:"Signin endpoint"
     })    
 })
 
-adminRouter.put("/",function(req,res) {    
+adminRouter.put("/course",function(req,res) {    
     res.json({
         message:"Signin endpoint"
     })    
 })
 
-adminRouter.get("/bulk",function(req,res) {    
+adminRouter.get("/course/bulk",function(req,res) {    
     res.json({
         message:"Signin endpoint"
     })    
